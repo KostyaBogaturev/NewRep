8e44e876b17fb993fb6682d72ce05d0d8a4c16d2
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-11-23 17:13:56 +0200

    Added infinity work of app and opportunity to exit after work with equation

--- Program.cs
+++ Program.cs
@@ -11,19 +11,30 @@ namespace QE
         static void Main(string[] args)
         {
             double indexA, indexB, indexC;
-            Console.Write("Enter index A: ");
-            indexA = double.Parse(Console.ReadLine());
-            Console.Write("Enter index B: ");
-            indexB = double.Parse(Console.ReadLine());
-            Console.Write("Enter index C: ");
-            indexC = double.Parse(Console.ReadLine());
-            Console.WriteLine($"{indexA}x^2 + {indexB}x + {indexC} = 0");
-            double discriminant = indexB * indexB - 4 * indexA * indexC;
-            double firstValue, secondValue;
-            firstValue = (-indexB - Math.Sqrt(discriminant)) / (2 * indexA);
-            secondValue = (-indexB + Math.Sqrt(discriminant)) / (2 * indexA);
-            Console.WriteLine($"x1= {firstValue} ,x2= {secondValue}");
-            Console.Read();
+            while (true)
+            {
+                Console.Write("Enter index A: ");
+                indexA = double.Parse(Console.ReadLine());
+                Console.Write("Enter index B: ");
+                indexB = double.Parse(Console.ReadLine());
+                Console.Write("Enter index C: ");
+                indexC = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{indexA}x^2 + {indexB}x + {indexC} = 0");
+                double discriminant = indexB * indexB - 4 * indexA * indexC;
+                double firstValue, secondValue;
+                firstValue = (-indexB - Math.Sqrt(discriminant)) / (2 * indexA);
+                secondValue = (-indexB + Math.Sqrt(discriminant)) / (2 * indexA);
+                Console.WriteLine($"x1= {firstValue} ,x2= {secondValue}");
+                Console.WriteLine("Enter '1' to close app");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        return;
+                    default:
+                        Console.Clear();
+                        break;
+                }
+            }
         }
     }
 }
